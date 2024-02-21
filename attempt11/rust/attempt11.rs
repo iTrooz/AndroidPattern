80702fdc75c3e25433a069ad167ccc55dde29af0
iTrooz <hey@itrooz.fr>
2024-02-21 16:38:52 +0100

    add cache for get_inbetween_points()

--- attempt11/rust/attempt11.rs
+++ attempt11/rust/attempt11.rs
@@ -5,6 +5,10 @@ const MIN_LEN: usize = 4;
 const MAX_LEN: usize = 7;
 make_all_points!(4); // create ALL_POINTS const. Pass SIZE as argument
 
+fn to_number(p: &(isize, isize)) -> isize {
+    p.0 * SIZE + p.1
+}
+
 fn is_close_int(n: f64) -> bool {
     let n = n.abs() % 1.0;
     let eps = 0.0001;
@@ -20,10 +24,15 @@ fn test_is_close_int() {
     assert!(!is_close_int(5.5));
 }
 
-fn get_inbetween_points(p1: &(isize, isize), p2: &(isize, isize)) -> Vec<(isize, isize)> {
+fn get_inbetween_points(c: &mut Context, p1: &(isize, isize), p2: &(isize, isize)) -> Vec<(isize, isize)> {
+    let id = to_number(p1) * SIZE*SIZE + to_number(p2);
+    if let Some(data) = c.inbetween_points_cache.get(id as usize) {
+        return data.clone()
+    }
+
     let xdiff = p2.0 - p1.0;
 
-    if xdiff == 0 {
+    let data: Vec<_> = if xdiff == 0 {
         let y_range = (p1.1.min(p2.1) + 1)..p1.1.max(p2.1);
         y_range.map(move |y| (p1.0, y)).collect()
     } else {
@@ -42,19 +51,26 @@ fn get_inbetween_points(p1: &(isize, isize), p2: &(isize, isize)) -> Vec<(isize,
                 }
             })
             .collect()
-    }
+    };
+    c.inbetween_points_cache[id as usize] = data.clone();
+    data
 }
 
 #[test]
 fn test_get_inbetween_points() {
-    assert_eq!(get_inbetween_points(&(0, 0), &(3, 3)), vec![(1, 1), (2, 2)]);
-    assert_eq!(get_inbetween_points(&(3, 3), &(0, 0)), vec![(1, 1), (2, 2)]);
-    assert_eq!(get_inbetween_points(&(1, 1), &(3, 5)), vec![(2, 3)]);
-    assert_eq!(get_inbetween_points(&(0, 0), &(0, 2)), vec![(0, 1)]);
-    assert_eq!(get_inbetween_points(&(0, 0), &(2, 0)), vec![(1, 0)]);
+    let c = &mut init_context();
+    assert_eq!(get_inbetween_points(c, &(0, 0), &(3, 3)), vec![(1, 1), (2, 2)]);
+    assert_eq!(get_inbetween_points(c, &(3, 3), &(0, 0)), vec![(1, 1), (2, 2)]);
+    assert_eq!(get_inbetween_points(c, &(1, 1), &(3, 5)), vec![(2, 3)]);
+    assert_eq!(get_inbetween_points(c, &(0, 0), &(0, 2)), vec![(0, 1)]);
+    assert_eq!(get_inbetween_points(c, &(0, 0), &(2, 0)), vec![(1, 0)]);
 }
 
-fn choose_next_point(used_points: &mut Vec<(isize, isize)>, last_point: (isize, isize)) -> isize {
+fn choose_next_point(
+    c: &mut Context,
+    used_points: &mut Vec<(isize, isize)>,
+    last_point: (isize, isize),
+) -> isize {
     let mut found_possibilities = 0;
 
     if used_points.len() >= MIN_LEN {
@@ -67,8 +83,7 @@ fn choose_next_point(used_points: &mut Vec<(isize, isize)>, last_point: (isize, 
     for p in ALL_POINTS {
         if !used_points.contains(&p) {
             let mut valid = true;
-
-            for between_p in get_inbetween_points(&last_point, &p) {
+            for between_p in get_inbetween_points(c, &last_point, &p) {
                 if !used_points.contains(&between_p) {
                     valid = false;
                     break;
@@ -77,7 +92,7 @@ fn choose_next_point(used_points: &mut Vec<(isize, isize)>, last_point: (isize, 
 
             if valid {
                 used_points.push(p);
-                found_possibilities += choose_next_point(used_points, p);
+                found_possibilities += choose_next_point(c, used_points, p);
                 used_points.pop();
             }
         }
@@ -86,13 +101,25 @@ fn choose_next_point(used_points: &mut Vec<(isize, isize)>, last_point: (isize, 
     found_possibilities
 }
 
+struct Context {
+    inbetween_points_cache: [Vec<(isize, isize)>; (SIZE.pow(4)) as usize],
+}
+
+fn init_context() -> Context {
+    const EMPTY_VEC: Vec<(isize, isize)> = vec![];
+    Context {
+        inbetween_points_cache: [EMPTY_VEC; (SIZE.pow(4)) as usize],
+    }
+}
+
 fn main() {
+    let mut c = init_context();
     let mut total = 0;
 
     for p in ALL_POINTS {
         println!("Starting start point {:?}", p);
         let mut used_points = vec![p];
-        total += choose_next_point(&mut used_points, p);
+        total += choose_next_point(&mut c, &mut used_points, p);
         println!("Finished start point {:?}", p);
     }
 
